222aedb99b151a1c1a9071e857e7f6e34246953d
Francisco Rodrigues <f.co.victor@hotmail.com>
2021-03-22 12:18:41 -0300

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,8 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+      title: 'Francisco Rodrigues',
+      description: 'Desenvolvedor de Software C# e Java.'
     }
 
     return (
@@ -22,12 +20,10 @@ class MyDocument extends Document {
           <meta property="og:site_name" content={meta.title} />
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
-          <meta property="og:image" content={meta.image} />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@yourname" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
         </Head>
         <body>
           <Main />
